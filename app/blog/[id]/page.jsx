bf283b67364b2e17d50f6342a6f1a60bae2d0198
WoW Developer <wowdeveloper@protonmail.com>
2023-01-29 19:35:54 +0530

    i’d update

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -5,36 +5,33 @@ const supabaseKey = process.env.SUPA_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export async function getData(id){
-  if(id==null||id.length==0){
-  id='af27c84d-32bb-4401-8e51-2569322ad88a'
-  }
-  try{
-  var { data: Blogs, error } = await supabase
+  
+  let { data: Blogs, error } = await supabase
   .from('Blogs')
   .select('*')
   .eq('uuid',id)
   var ss ={data:Blogs};
-  return ss[0]
-  }
-  catch(error){
-  console.log(error)
-  }
+  const yy=ss?.data
+  return yy
+
 }
 
 export default async function Page({params}) {
-  try{
-  var xx= await getData(params.id);
-  console.log(xx)
+ 
+  const data = await getData(params.id);
   
   return (
+  
     <div className="h-full w-full mx-auto p-4 max-w-[600px] pt-16 flex flex-col ">
-                <div className="h-64 w-full p-2 rounded-lg" style={{ backgroundImage: `url(${xx.image})` }}/>
+             {data.map((xx)=>(<div key={xx}>
+            
+               <div className="h-64 w-full p-2 rounded-lg" style={{ backgroundImage: `url(${xx.image})` }}/>
                 <div className="text-3xl capitalize mt-2 font-semibold">{xx.title}</div>
                 <div className="text-xl mt-1">Published on :- {xx.date}</div>
                 <div className="text-2xl mt-1">{xx.description}</div>
+             
+             </div>
+             ))  
     </div>
-  )}
-  catch(error){
-  console.log(error)
-  }
+  )
 }
