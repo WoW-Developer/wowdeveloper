377e8ae81daba1664030ec2ccaef515dd6e5e0d4
WoW Developer <wowdeveloper@protonmail.com>
2023-01-29 10:58:08 +0530

    a

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -4,12 +4,12 @@ const supabaseUrl = process.env.SUPA_URL
 const supabaseKey = process.env.SUPA_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-const fetchSingleBlogData = async (id) => {
+const fetchSingleBlogData = async (params.id) => {
 
   let { data: Blogs, error } = await supabase
   .from('Blogs')
   .select('*')
-  .eq('uuid',id!=null?id:1)
+  .eq('uuid',params.id)
   var ss ={data:Blogs};
   var xx= await ss!=null?ss.data[0]:[];
   return xx
@@ -17,7 +17,7 @@ const fetchSingleBlogData = async (id) => {
 
 export default async function Page({params}) {
   
-  const query = await fetchSingleBlogData(params.id!=null?params.id:'');
+  const query = await fetchSingleBlogData(params.id);
   return (
     <div className="h-full w-full mx-auto p-4 max-w-[600px] pt-16 flex flex-col ">
                 <div className="h-64 w-full p-2 rounded-lg" style={{ backgroundImage: `url(${query.image})` }}/>
