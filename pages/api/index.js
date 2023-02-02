d0778d3a7f0c307e463e13f4856157f31317c426
WoW Developer <wowdeveloper@protonmail.com>
2023-02-02 16:12:25 +0530

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -2,7 +2,12 @@
 
 export default function handler(req, res) {
   
-    res.status(200).send(data)
+    res.status(200).send(<html> 
+ <body> 
+ <h1>Hello World</h1> 
+ <p>I'm hosted with ApI Pages.</p> 
+ </body> 
+ </html>)
      
   }
   
