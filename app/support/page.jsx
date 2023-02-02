e011bc448e6f65e94d29f42f78194bb96e354266
WoW Developer <wowdeveloper@protonmail.com>
2023-02-02 04:33:25 +0000

    some changes

--- app/support/page.jsx
+++ app/support/page.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import Razorpay from 'razorpay'
+
 
 const page = () => {
+var instance = new Razorpay({
+  key_id:process.env.RAZOR_KEY,
+  key_secret:process.env.RAZOR_SECRET
+})
+
+
   return (
     <div className="h-full w-full flex items-center justify-center">
       Suport Page
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,12 +1,8 @@
 
 
 export default function handler(req, res) {
-    res.status(200).json({ 
-      title: req.query.title,
-      desc: req.query.description,
-      image: req.query.image,
-      date: req.query.date,
-      uuid: req.query.uuid,
-     })
+  
+    res.status(200).send(data)
+     
   }
   
